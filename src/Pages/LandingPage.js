fa61f5cdf5207b6eb148d973e6ae585b9d233b68
captainsesman <captainsesman@gmail.com>
2023-01-14 13:06:00 +0100

    Firt Installation and Project Setup

---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,13 @@
+import Menu from "../Menu";
+
+const LandingPage = () => {
+  return (
+    <>
+    <Menu></Menu>
+      {/* <SideBar></SideBar>
+      <p onMouseDown={() => alert("Broooooo!")}>{name}</p> */}
+    </>
+  );
+};
+
+export { LandingPage };
